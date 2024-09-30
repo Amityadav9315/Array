9818a7a18a440eab88f1b93e9c232d5dbadcb95c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-09-30 18:37:09 +0530

    Smallest

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Reverse {
+package Array;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = {12, 45, 65, 34, 78};
+        int n = a.length;
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
--- src/Smallest.java
+++ src/Smallest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Smallest {
+public class Smallest {
+    public static void main(String[] args) {
+        int arr[] = {34, 56, 45, 67, 80, 43, 65};
+        int n = arr.length;
+
+        // Initialize min with the first element of the array
+        int min = arr[0];
+
+        // Iterate through the array starting from the second element
+        for(int i = 1; i < n; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Smallest number in the array: "+min);
+}
 }
