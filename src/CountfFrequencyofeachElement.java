e0a4db6361eaad7881a77e89e91171980ead591b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-04 19:44:24 +0530

    Countfrequencyofeachelement

--- src/CountfFrequencyofeachElement.java
+++ src/CountfFrequencyofeachElement.java
@@ -4,12 +4,21 @@ public class CountfFrequencyofeachElement {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        int count=1;
         int[] arr=new int[n];
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
 
         }
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n-1; i++){
+            for(int j=1; j<n; j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+
+            }
+            System.out.println(arr[i]+" "+count);
+            count=0;
 
         }
     }
