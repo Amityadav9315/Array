ccd1812d67f94c42e016d1d2af18b2408597b889
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-04 19:59:29 +0530

    Countfrequencyofeachelement

--- src/CountfFrequencyofeachElement.java
+++ src/CountfFrequencyofeachElement.java
@@ -1,25 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
+class CountFrequencyofeachelement
+{
+    public static void countFreq(int arr[], int n)
+    {
+        boolean visited[] = new boolean[n];
+        Arrays.fill(visited, false);
 
-public class CountfFrequencyofeachElement {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int count=1;
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
 
-        }
-        for(int i=0; i<n-1; i++){
-            for(int j=1; j<n; j++){
-                if(arr[i]==arr[j]){
+            if (visited[i] == true)
+                continue;
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
                     count++;
                 }
-
             }
-            System.out.println(arr[i]+" "+count);
-            count=0;
-
+            System.out.println(arr[i] + " occurs " + count +" times ");
         }
+    }
+
+    // Driver code
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        countFreq(arr, n);
     }
 }
