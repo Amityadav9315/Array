45e61f427ab9d32cb65b11cfc3a5fe7523a6e771
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-04 19:55:36 +0530

    CopyingAnarray

---
+++ src/CopyingAnarray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CopyingAnarray {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+
+        }
+        int[] b=new int[n];
+        for(int i=0; i<n; i++){
+            b[i]=arr[i];
+        }
+        for(int x:b){
+            System.out.print(x+" ");
+        }
+    }
+}
