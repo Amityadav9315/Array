e87af65ab717bcb2d858d68ed8929da3432dc4f4
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-12 20:59:18 +0530

    AddElementInArray

---
+++ src/AddanElementinArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AddanElementinArray {
+
+
+    static void Add(int arr[], int n, int s) {
+        for (int i = arr.length - 2; i >= 2; i--) {
+            arr[i+1]=arr[i];
+
+
+        }
+
+        arr[2] = s;
+        for (int x : arr) {
+            System.out.println(x);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+
+        // Fill the array with user input
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int s = 60;
+        Add(arr, n, s);
+    }
+}
