7002354afb546be0ae20d1e54bda974fd625b87d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-13 16:04:21 +0530

    Repeating element

--- src/FindAllrepeatingElement.java
+++ src/FindAllrepeatingElement.java
@@ -2,34 +2,39 @@ import java.util.Scanner;
 
 public class FindAllrepeatingElement {
 
-    static void Repeat(int arr[],int n,int c){
-        for(int i=0; i<n-1; i++){
-            for(int j=i; j<arr.length-1; j++) {
-                if (arr[i] == arr[j + 1]) {
-                    c++;
+    static void Repeat(int arr[], int n) {
+        boolean[] isPrintedarr = new boolean[n];  // To track printed elements
+
+        for (int i = 0; i < n - 1; i++) {
+            int c = 0;  // Reset counter for each element
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    c++;  // Count occurrences of arr[i]
                 }
             }
-                if(c>0){
-                    System.out.print(arr[i]+ " ");
+
+            if (c > 0 && !isPrintedarr[i]) {
+                System.out.print(arr[i] + " ");
+                // Mark duplicates so they won't be printed again
+                for (int k = i; k < n; k++) {
+                    if (arr[k] == arr[i]) {
+                        isPrintedarr[k] = true;
+                    }
                 }
-
+            }
         }
-
-
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int c=0;
-        int[] arr=new int[n];
-        for(int i=0;  i<n; i++){
-            arr[i]=sc.nextInt();
-
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        Repeat(arr,n,c);
 
+        Repeat(arr, n);
     }
-
 }
