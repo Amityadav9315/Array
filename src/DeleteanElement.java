9a45376314ae27968adc2bbe80de31a52c6f1a79
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-15 10:17:37 +0530

    DeletionElement

--- src/DeleteanElement.java
+++ src/DeleteanElement.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class DeleteanElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println();
         int n = sc.nextInt();
         int temp = 0;
         int[] arr = new int[n];
